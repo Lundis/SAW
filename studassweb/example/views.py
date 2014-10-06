024fa0b9ad76eeefbd9ebd22c49d5c93cfa91739
Lundis <lundis.bah@gmail.com>
2014-10-06 22:25:02 +0300

    Wrapper all strings in i18n-calls.

--- studassweb/example/views.py
+++ studassweb/example/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils.translation import ugettext as _
 
 team_members = ['Lundis', 'Magnus', 'Johnny', 'Oscar', 'J', 'Asis']
 # Create your views here.
@@ -6,7 +7,7 @@ def choose(request):
 	return render(request, 'example/choose.html', {'members' : team_members})
 
 def result(request, winner):
-	vars = {'winner' : 'No one'}
+	vars = {'winner' : _('No one')}
 	try:
 		result = int(winner)
 		# make sure that it's a valid option
