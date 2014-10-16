3549cb0eca1785958fd3363b1f307d549f414bcf
Lundis <lundis.bah@gmail.com>
2014-10-17 00:29:03 +0300

    Johnny's changes

--- studassweb/polls/models.py
+++ studassweb/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Poll_question(models.Model):
+    question_text= models.CharField(max_length=300)
+    publication_date = models.DateTimeField('Date published')
+    # expiration date
+
+class User_choice(models.Model):
+    question = models.ForeignKey(Poll_question)
+    choice_text = models.CharField(max_length=300)
+    vote_amount = models.IntegerField(default=0)
+
