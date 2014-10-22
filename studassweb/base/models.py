6bf8ba70c0489bc518e57c76579ab5388283b327
Lundis <lundis.bah@gmail.com>
2014-10-22 11:52:37 +0300

    More work on install.

--- studassweb/base/models.py
+++ studassweb/base/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from solo.models import SingletonModel
+from menu.models import Menu
 
 class SiteConfiguration(SingletonModel):
     association_name = models.CharField(max_length=100, default='Site name')
+    main_menu = models.ForeignKey(Menu, blank=True, null=True)
+
+class disabled_module(models.Model):
+    app_name = models.CharField(max_length=50)
--- studassweb/install/views.py
+++ studassweb/install/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.utils.translation import ugettext as _
 from install.forms import AssociationForm
+
+
+def first_letter_to_upper(str):
+    """
+    :param str:
+    :return: str with the first letter in upper case
+    """
+    return str[0].upper() + str[1:]
+
+stages = [[s, _(first_letter_to_upper(s))] for s in ["welcome", "association", "modules", "menu", "finished"]]
 
 @login_required
 def welcome(request):
     context = {'current_url': request.get_full_path(),
-               'current': 'welcome'}
+               'stages': stages,
+               'current_stage_index': 0,
+               'current_stage': stages[0]}
     return render(request, 'install/base.html', context)
 
 @login_required
@@ -22,25 +35,34 @@ def association(request):
 
 
     context = {'previous': 'welcome',
-               'current': 'association',
-               'form': form,}
+               'form': form,
+               'stages': stages,
+               'current_stage_index': 1,
+               'current_stage': stages[1]}
     return render(request, 'install/base.html', context)
 
 @login_required
 def modules(request):
-
+    #TODO: get all available modules and list them and let the user choose which he wants
     context = {'previous': 'association',
-               'current': 'modules'}
+               'stages': stages,
+               'current_stage_index': 2,
+               'current_stage': stages[2]}
     return render(request, 'install/base.html', context)
 
 @login_required
 def menu(request):
+    # TODO: get all available menu items and let the user choose which he wants, and in which order.
     context = {'previous': 'modules',
-               'current': 'menu'}
+               'stages': stages,
+               'current_stage_index': 3,
+               'current_stage': stages[3]}
     return render(request, 'install/base.html', context)
 
 @login_required
 def finished(request):
     context = {'previous': 'menu',
-               'current': 'finished'}
+               'stages': stages,
+               'current_stage_index': 4,
+               'current_stage': stages[4]}
     return render(request, 'install/base.html', context)
