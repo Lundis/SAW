93d2f8f796a2d160f938450b2b004e7e6e6ac443
Lundis <lundis.bah@gmail.com>
2014-10-28 19:08:51 +0200

    Added Gallery skeletons according to the design document.

--- studassweb/gallery/urls.py
+++ studassweb/gallery/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'views.home', name='home'),
+    url(r'^$', 'gallery.views.view_gallery', name='view_gallery'),
+    url(r'^album/(?P<id>\d+)$', 'gallery.views.view_album', name='view_album'),
+    url(r'^album/(?P<id>\d+)/edit$', 'gallery.views.edit_album', name='edit_album'),
+    url(r'^album/create$', 'gallery.views.create_album', name='create_album'),
+    url(r'^picture/(?P<id>\d+)$', 'gallery.views.view_picture', name='view_picture'),
+    url(r'^picture/(?P<id>\d+)/edit$', 'gallery.views.view_picture', name='view_picture'),
 )
