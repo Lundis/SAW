c4e5cf83de99dc03513d74d24b9ea25b43ce69b4
Lundis <lundis.bah@gmail.com>
2014-11-04 18:14:42 +0200

    Added a register.py file to the polls module so that Johnny can start viewing his stuff :D

---
+++ studassweb/polls/register.py
@@ -0,0 +1,10 @@
+from menu.models import MenuItem
+
+def get_menu_items():
+    return [MenuItem.get_or_create("polls", "Polls", "/polls/", MenuItem.NONE)]
+
+def get_urls():
+    """
+    :returns: A tuple of regexes describing what URLs the top-level URL dispatcher should associate with this module
+    """
+    return (r"^polls/",)
