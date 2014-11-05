8dc029f792657b4635fecdab1871a1fdf392f032
Lundis <lundis.bah@gmail.com>
2014-11-05 16:50:16 +0200

    Improved the install module by refactoring the progress "menu" using an inclusion tag.

---
+++ studassweb/install/templatetags/install_progress_bar.py
@@ -0,0 +1,15 @@
+from django.template import RequestContext, Library
+from ..models import InstallProgress
+
+register = Library()
+
+@register.inclusion_tag('install/progress_bar.html', takes_context=True)
+def display_progress_bar(context, active_tab):
+    """
+    Renders the installation wizard's progress bar
+    :param active_tab: The active installation phase
+    :return:
+    """
+    progress, created = InstallProgress.objects.get_or_create()
+
+    return {'progress': progress, 'active_tab': active_tab, 'user': context['user']}
--- studassweb/install/views.py
+++ studassweb/install/views.py
@@ -1,28 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.utils.translation import ugettext as _
 from django.conf import settings
 from install.forms import AssociationForm, ModulesForm, MenuForm
 from users.forms import LoginForm
 from menu.logic import get_all_menu_items
 from menu.models import Menu, MenuItem
+from .models import InstallProgress
 
-
-def first_letter_to_upper(str):
-    """
-    :param str:
-    :return: str with the first letter in upper case
-    """
-    return str[0].upper() + str[1:]
-
-# make an array of arrays, where each entry represents [stage_name, user_friendly_name]
-stages = [[s, _(first_letter_to_upper(s))] for s in ["welcome", "association", "modules", "menu", "finished"]]
 
 def welcome(request):
-    context = {'stages': stages,
-               'current_stage_index': 0,
-               'current_stage': stages[0]}
+    context = {}
     if not request.user.is_authenticated():
         login_form = LoginForm(request.POST or None)
         if login_form.is_valid():
@@ -38,11 +26,9 @@ def association(request):
     form = AssociationForm(request.POST or None)
     if form.is_valid():
         form.apply()
+        InstallProgress.site_name_set()
         return HttpResponseRedirect('modules')
-    context = {'form': form,
-               'stages': stages,
-               'current_stage_index': 1,
-               'current_stage': stages[1]}
+    context = {'form': form}
     return render(request, 'install/assoc.html', context)
 
 @login_required
@@ -50,12 +36,10 @@ def modules(request):
     form = ModulesForm(request.POST or None, modules=settings.OPTIONAL_APPS)
     if form.is_valid():
         form.apply()
+        InstallProgress.modules_set()
         return HttpResponseRedirect('menu')
 
-    context = {'form': form,
-               'stages': stages,
-               'current_stage_index': 2,
-               'current_stage': stages[2]}
+    context = {'form': form}
     return render(request, 'install/modules.html', context)
 
 @login_required
@@ -63,6 +47,7 @@ def menu(request):
     form = MenuForm(request.POST or None)
     if form.is_valid():
         form.apply()
+        InstallProgress.menu_set()
         return HttpResponseRedirect('finished')
 
     available_items = get_all_menu_items()
@@ -78,10 +63,7 @@ def menu(request):
     context = {'menu_items': menu_items,
                'login_items': login_items,
                'available_items': available_items,
-               'form': form,
-               'stages': stages,
-               'current_stage_index': 3,
-               'current_stage': stages[3]}
+               'form': form}
     return render(request, 'install/menu.html', context)
 
 def get_main_menu_items():
@@ -112,7 +94,6 @@ def get_login_menu_items():
 
 @login_required
 def finished(request):
-    context = {'stages': stages,
-               'current_stage_index': 4,
-               'current_stage': stages[4]}
+    InstallProgress.finish()
+    context = {}
     return render(request, 'install/finished.html', context)
