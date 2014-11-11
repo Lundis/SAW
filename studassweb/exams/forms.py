3166998878b06997552439687eb74e87724a2745
mgulin <mgulin@abo.fi>
2014-11-11 22:09:40 +0200

    Now possible to add courses and examinators

--- studassweb/exams/forms.py
+++ studassweb/exams/forms.py
@@ -5,8 +5,25 @@ from exams.models import *
 
 class ExamForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(ExamForm, self).__init__(*args, **kwargs)
+        self.fields['course_id'].label_from_instance = lambda obj: "%s" % obj.name
+        self.fields['examinator'].label_from_instance = lambda obj: "%s" % obj.name
+
     class Meta:
         model = SingleExam
         fields = ('photo_id', 'course_id', 'ocr', 'exam_date', 'examinator')
 
 
+class ExaminatorForm(forms.ModelForm):
+
+    class Meta:
+        model = Examinator
+        fields = ('name',)
+
+
+class CourseForm(forms.ModelForm):
+
+    class Meta:
+        model = Course
+        fields = ('name',)
--- studassweb/exams/views.py
+++ studassweb/exams/views.py
@@ -11,45 +11,42 @@ Don't use this as an example.
 def main(request):
     return HttpResponseNotFound('main(): function not implemented')
 
+
 def view_exam(request,exam_id):
-    exams = SingleExam.objects.filter(id=exam_id)
-    if len(exams) > 0:
-        exam = SingleExam.getById(exam_id)
-        return render(request,'exam/view_exam.html',
+    try:
+        exam = SingleExam.objects.get(id=exam_id)
+        return render(request,'view_exam.html',
             {'id': exam.id, 'photo_id': exam.photo_id, 'course_id': exam.photo_id,
             'ocr': exam.ocr, 'exam_date': exam.exam_date, 'examinator': exam.examinator},)
-    else:
+    except SingleExam.DoesNotExist:
         return HttpResponseNotFound('No exam with that id found')
 
 
 def view_examinator(request,examinator_id):
     #TODO show all exams by examinator
-    examinators = SingleExam.objects.filter(id=examinator_id)
-    if len(examinators) > 0:
-        examinator = SingleExam.getById(examinator_id)
-        return render(request,'exam/view_examinator.html',
+    try:
+        examinator = Examinator.objects.get(id=examinator_id)
+        return render(request,'view_examinator.html',
             {'id': examinator.id, 'name': examinator.name},)
-    else:
+    except Examinator.DoesNotExist:
         return HttpResponseNotFound('No examinator with that id found')
 
 
 def view_course(request,course_id):
     #TODO show all exams from course
-    courses = SingleExam.objects.filter(id=course_id)
-    if len(courses) > 0:
-        course = SingleExam.getById(course_id)
-        return render(request,'exam/view_course.html',
+    try:
+        course = Course.objects.get(id=course_id)
+        return render(request,'view_course.html',
             {'id': course.id, 'name': course.name},)
-    else:
+    except Course.DoesNotExist:
         return HttpResponseNotFound('No course with that id found')
 
 
 def add_edit_exam(request, exam_id=-1):
     form = ExamForm(request.POST or None)
     if form.is_valid():
-        form.save()
-        #TODO return to saved exam
-        return HttpResponseRedirect('/exams/')
+        tmp = form.save()
+        return HttpResponseRedirect('/exams/exam/' + str(tmp.id))
     try:
         exam = SingleExam.objects.get(id=exam_id)
         form = ExamForm(exam)
@@ -60,10 +57,31 @@ def add_edit_exam(request, exam_id=-1):
     return render(request, 'add_edit_exam.html', context)
 
 
+def add_edit_examinator(request,examinator_id=-1):
+    form = ExaminatorForm(request.POST or None)
+    if form.is_valid():
+        tmp = form.save()
+        return HttpResponseRedirect('/exams/examinator/' + str(tmp.id))
+    try:
+        examinator = Examinator.objects.get(id=examinator_id)
+        form = ExaminatorForm(examinator)
+    except Examinator.DoesNotExist:
+        pass
 
-def add_edit_examinator(request,examinator_id):
-    return HttpResponseNotFound('add_edit__examinator(): function not implemented')
+    context = {'form': form}
+    return render(request, 'add_edit_examinator.html', context)
 
 
-def add_edit_course(request,course_id):
-    return HttpResponseNotFound('add_edit__course(): function not implemented')
+def add_edit_course(request,course_id=-1):
+    form = CourseForm(request.POST or None)
+    if form.is_valid():
+        tmp = form.save()
+        return HttpResponseRedirect('/exams/course/' + str(tmp.id))
+    try:
+        course = Examinator.objects.get(id=course_id)
+        form = CourseForm(course)
+    except Examinator.DoesNotExist:
+        pass
+
+    context = {'form': form}
+    return render(request, 'add_edit_course.html', context)
