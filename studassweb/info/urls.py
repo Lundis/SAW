c1420f98d4a327c34d194c5cec1c2008e0bd540d
Lundis <lundis.bah@gmail.com>
2014-11-27 21:36:25 +0200

    bugfixes in the info module.

--- studassweb/info/urls.py
+++ studassweb/info/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'info.views.main', name='info_view_categories'),
-    url(r'^cat/new$', 'info.views.edit_category', name='info_new_categories'),
+    url(r'^cat/new$', 'info.views.edit_category', name='info_new_category'),
     url(r'^cat/(?P<category_id>\d+)/$', 'info.views.view_category', name='info_view_category'),
     url(r'^cat/(?P<category_id>\d+)/edit$', 'info.views.edit_category', name='info_edit_category'),
-    url(r'^cat/(?P<category_id>\d+)/delete$', 'info.views.edit_page', name='info_create_category'),
+    url(r'^cat/(?P<category_id>\d+)/delete$', 'info.views.delete', name='info_delete_category'),
     url(r'^page/(?P<page_id>\d+)$', 'info.views.view_page', name='info_view_page'),
-    url(r'^page/(?P<category_id>\d+)new$', 'info.views.edit_page', name='info_new_page'),
+    url(r'^page/(?P<category_id>\d+)/new$', 'info.views.edit_page', name='info_new_page'),
     url(r'^page/(?P<page_id>\d+)/edit$', 'info.views.edit_page', name='info_edit_page'),
     url(r'^page/(?P<page_id>\d+)/delete', 'info.views.delete', name='info_delete_page')
 )
