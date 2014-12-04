d867728c48f604b83ef743072717a50a52365c63
mgulin <mgulin@abo.fi>
2014-12-04 15:06:03 +0200

    fixed reset_db.bat. I fucking hate windows cmd

--- studassweb/reset_db.bat
+++ studassweb/reset_db.bat
@@ -1,6 +1,15 @@
-del db.sqlite3 && ^
+@echo off
+
+REM delete db
+del db.sqlite3
+
+REM since del is not returning an errorlevel (WTF!!1!)
+REM check manually if file was actually removed or go to end
+@dir db.sqlite3 >NUL 2>&1 && goto :eof
+
+REM do django stuff
 python manage.py makemigrations && ^
 python manage.py migrate && ^
 python manage.py createsuperuser && ^
-python manage.py runserver
+python manage.py runserver 
 pause
