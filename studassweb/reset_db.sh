d3f9f99ad8f171b7d90c9a275f99aaeaf67304e2
Lundis <lundis.bah@gmail.com>
2015-01-04 23:18:20 +0200

    Reset script didn't work on *nix when the database file doesn't exist.

--- studassweb/reset_db.sh
+++ studassweb/reset_db.sh
@@ -1,1 +1,1 @@
-rm db.sqlite3 && python manage.py makemigrations && python manage.py migrate && python manage.py createsuperuser && python manage.py runserver
+rm -f db.sqlite3 && python manage.py makemigrations && python manage.py migrate && python manage.py createsuperuser && python manage.py runserver
