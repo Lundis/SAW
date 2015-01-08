173db1647c43129168b3da05066974f0cb729971
Lundis <lundis.bah@gmail.com>
2015-01-08 12:17:23 +0200

    Added an ajax api for putting users in default groups

--- studassweb/users/urls.py
+++ studassweb/users/urls.py
@@ -6,5 +6,9 @@ urlpatterns = patterns('',
     url(r'^register$', 'users.views.register', name='users_register'),
     url(r'^register_thanks$', 'users.views.register_thanks', name='users_register_thanks'),
     url(r'^profile/(?P<username>[a-zA-Z0-9_]{3,30})$', 'users.views.view_profile', name='users_view_profile'),
-    url(r'^verify_email/(?P<code>[a-z0-9]{32})', 'users.views.verify_email', name='users_verify_email')
+    url(r'^verify_email/(?P<code>[a-z0-9]{32})', 'users.views.verify_email', name='users_verify_email'),
+
+    url(r'ajax/set_default_group/(?P<username>[a-zA-Z0-9_]{3,30})/(?P<group_name>[a-zA-Z0-9_ ]{3,30})$',
+        'users.ajax.set_default_group',
+        name='users_ajax_set_default_group'),
 )
