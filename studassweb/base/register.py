cad89290f6734c12c7abc46933f07fbdf06efb84
Lundis <lundis_lundis@hotmail.com>
2015-01-22 14:12:59 +0200

    Some work on comments.

--- studassweb/base/register.py
+++ studassweb/base/register.py
@@ -2,7 +2,8 @@ from users.groups import GUEST, BOARD_MEMBER, LOGGED_ON, WEBMASTER
 from menu.models import MenuItem
 from users.models import SAWPermission
 
-VIEW_COMMENTS = "can_view_comments"
+VIEW_PUBLIC_COMMENTS = "can_view_public_comments"
+VIEW_MEMBER_COMMENTS = "can_view_member_comments"
 CAN_COMMENT = "can_comment"
 FORCE_COMMENT = "can_force_comment"
 EDIT_THEME = "can_edit_themes"
@@ -13,8 +14,9 @@ def get_permissions():
     :return: a list of tuples containing the permissions of this module and their default group
     """
     return (
-        (VIEW_COMMENTS, GUEST, "Can view comments on items with comments"),
-        (CAN_COMMENT, LOGGED_ON, "Can comment on items with comments enabled"),
+        (VIEW_PUBLIC_COMMENTS, GUEST, "Can view comments on items with public comments"),
+        (VIEW_MEMBER_COMMENTS, GUEST, "Can view comments on items with member comments"),
+        (CAN_COMMENT, LOGGED_ON, "Can comment on items with comments enabled.  This is further restricted by the view permissions for comments."),
         (FORCE_COMMENT, BOARD_MEMBER, "Can comment to items with comments disabled"),
         (EDIT_THEME, WEBMASTER, "Can change the overall theme of the site")
     )
--- studassweb/users/templatetags/sawp.py
+++ studassweb/users/templatetags/sawp.py
@@ -1,6 +1,7 @@
 from django.template import Library
 from users.permissions import has_user_perm
 from base.utils import get_str_from_module
+from base.register import FORCE_COMMENT
 import logging
 
 logger = logging.getLogger(__name__)
@@ -34,6 +35,25 @@ def can_edit_object(context, obj):
 @register.assignment_tag(takes_context=True)
 def can_view_object(context, obj):
     access = obj.can_view(context['user'])
+    logger.debug("can_view_object(context, %s): %s", obj, access)
+    if hasattr(obj, "permission"):
+        logger.debug("user: %s, permission: %s", context['user'], obj.permission)
+    return access
+
+
+@register.assignment_tag(takes_context=True)
+def can_view_comments(context, obj):
+    access = obj.can_view_comments(context['user']) and obj.comments_enabled
+    logger.debug("can_view_comments(context, %s): %s", obj, access)
+    return access
+
+
+@register.assignment_tag(takes_context=True)
+def can_comment(context, obj):
+    if not can_view_comments(context, obj):
+        logger.debug("user %s cannot comment on %s (cannot read)", context['user'], obj)
+        return False
+    access = obj.can_comment(context['user']) and obj.comments_enabled
     logger.debug("can_view_object(context, %s): %s", obj, access)
     if hasattr(obj, "permission"):
         logger.debug("user: %s, permission: %s", context['user'], obj.permission)
