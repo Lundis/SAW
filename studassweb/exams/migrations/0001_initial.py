e60216d9aee22f35b92b75217533f56f36a43bf4
Lundis <lundis_lundis@hotmail.com>
2015-01-28 11:35:53 +0200

    Reset the migrations. RESET YOUR DATABASES!

--- studassweb/exams/migrations/0001_initial.py
+++ studassweb/exams/migrations/0001_initial.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('gallery', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -16,6 +18,17 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
                 ('name', models.CharField(max_length=100)),
+                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, null=True, blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ExamFile',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('image', models.ImageField(upload_to='exams_files')),
             ],
             options={
             },
@@ -26,6 +39,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
                 ('name', models.CharField(max_length=100)),
+                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, null=True, blank=True)),
             ],
             options={
             },
@@ -35,14 +49,20 @@ class Migration(migrations.Migration):
             name='SingleExam',
             fields=[
                 ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
-                ('ocr', models.TextField()),
+                ('ocr', models.TextField(blank=True)),
                 ('exam_date', models.DateTimeField()),
-                ('course_id', models.ForeignKey(to='exams.Course')),
-                ('examinator', models.ForeignKey(to='exams.Examinator')),
-                ('photo_id', models.ForeignKey(to='gallery.Photo')),
+                ('course_id', models.ForeignKey(to='exams.Course', on_delete=django.db.models.deletion.PROTECT)),
+                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, null=True, blank=True)),
+                ('examinator', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='exams.Examinator', null=True, blank=True)),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='examfile',
+            name='exam_id',
+            field=models.ForeignKey(to='exams.SingleExam'),
+            preserve_default=True,
         ),
     ]
