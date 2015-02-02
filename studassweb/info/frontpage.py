842788aa357064b784543138f7c524b86bb6050f
Lundis <lundis_lundis@hotmail.com>
2015-02-02 13:34:09 +0200

    Prepared the info module for frontpage integration.

---
+++ studassweb/info/frontpage.py
@@ -0,0 +1,20 @@
+
+
+class InfoFrontPageItem():
+
+    def __init__(self, page):
+        self.page = page
+
+    def title(self):
+        return self.page.title
+
+    def contents(self):
+        return self.page.text
+
+    @staticmethod
+    def show_in_side_bar():
+        return True
+    
+    @staticmethod
+    def show_in_main_bar():
+        return True
