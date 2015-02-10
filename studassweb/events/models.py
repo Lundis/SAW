55aa3f8a4866587450645162990c43d0a86b479c
mgulin <mgulin@abo.fi>
2015-02-11 00:27:55 +0200

    Events module: Added stuff to database models, added edit signup functionality,

--- studassweb/events/models.py
+++ studassweb/events/models.py
@@ -2,16 +2,31 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from datetime import datetime
+from .register import CAN_CREATE_EVENTS
+from users import permissions
+
+
+# This should maybe be put in base or something
+class MultiInputField(models.CharField):
+    description = "This is magic"
+
+    def __init__(self, *args, **kwargs):
+        super(MultiInputField, self).__init__(*args, **kwargs)
+        #self.validators.append(validators.MaxLengthValidator(self.max_length))
 
 
 #This is an actual event, for example a Christmas party
+# TODO is it public, for members, or for board members only?
+# TODO slug for urls
 class Event(models.Model):
     title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
     text = models.TextField(max_length=500)
     start = models.DateTimeField()
     stop = models.DateTimeField()
     author = models.ForeignKey(User)
     signup_deadline = models.DateTimeField()
+    permission = models.CharField(max_length=100, blank=True, null=True)  # Permission needed to see and attend
 
     def __str__(self):
         return str(self.title)
@@ -22,6 +37,7 @@ class Event(models.Model):
 
 #Each user which signs up creates one of these
 #We need both user and name as we need to allow non-signed in users to sign up
+# TODO we need to save the auth_codes somehow, to ensure that a new signup doesn't get the same code as a delete one
 class EventSignup(models.Model):
     event = models.ForeignKey(Event)
     user = models.ForeignKey(User, blank=True, null=True)
@@ -32,7 +48,16 @@ class EventSignup(models.Model):
     diet = models.CharField(max_length=200, null=True, blank=True)
     other = models.CharField(max_length=200, null=True, blank=True)
     created = models.DateTimeField(default=datetime.now, blank=True)
-    delete_confirmation_code = models.CharField(max_length=32, unique=True)# TODO rename this as we will use it for editing as well
+    auth_code = models.CharField(max_length=32, unique=True)  # Edit and delete for anonymous users
+
+    def user_can_edit(self, user):
+        print(self.name)
+        print(self.user)
+        print(user)
+        if self.user == user or permissions.has_user_perm(user, CAN_CREATE_EVENTS):
+            return True
+        else:
+            return False
 
 
 class EventItem(models.Model):
@@ -44,8 +69,20 @@ class EventItem(models.Model):
 
 # This is for setting which items can be set when signing up to event
 class ItemInEvent(models.Model):
+    TYPE_BOOL = 'B'
+    TYPE_STR = 'S'
+    TYPE_INT = 'I'
+    TYPE_CHOICE = 'C'
+    TYPE_CHOICES = (
+        (TYPE_BOOL, 'Boolean'),
+        (TYPE_STR, 'String'),
+        (TYPE_INT, 'Integer'),
+        (TYPE_CHOICE, 'Choice'),
+    )
+
     event = models.ForeignKey(Event)
     item = models.ForeignKey(EventItem)
+    type = models.CharField(max_length=1, choices=TYPE_CHOICES, default=TYPE_INT)
 
     def __str__(self):
         return str("{0} is enabled in {1}".format(self.item.name, self.event.title))
@@ -56,6 +93,9 @@ class ItemInSignup(models.Model):
     signup = models.ForeignKey(EventSignup)
     item = models.ForeignKey(EventItem)
     amount = models.IntegerField()
+    value = MultiInputField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return str("{0} signed up for {1} {2}".format(self.signup.name, self.amount, self.item.name))
+
+
