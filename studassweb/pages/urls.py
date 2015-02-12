525bdebc0312e413c38e3781c6dfd746f66831e7
Lundis <lundis.bah@gmail.com>
2015-02-12 11:32:09 +0200

    Added a new page button to pages main and fixed the navigator when viewing a page without a category

--- studassweb/pages/urls.py
+++ studassweb/pages/urls.py
@@ -5,14 +5,15 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'pages.views.main', name='pages_view_categories'),
 
-    url(r'^category/new$', 'pages.views.edit_category', name='pages_new_category'),
+    url(r'^new_category$', 'pages.views.edit_category', name='pages_new_category'),
     url(r'^category/(?P<slug>[\w-]+)/$', 'pages.views.view_category', name='pages_view_category'),
-    url(r'^category/(?P<category_id>\d+)/edit$', 'pages.views.edit_category', name='pages_edit_category'),
-    url(r'^category/(?P<category_id>\d+)/delete$', 'pages.views.delete_category', name='pages_delete_category'),
+    url(r'^edit_category/(?P<category_id>\d+)$', 'pages.views.edit_category', name='pages_edit_category'),
+    url(r'^delete_category/(?P<category_id>\d+)$', 'pages.views.delete_category', name='pages_delete_category'),
 
     url(r'^page/(?P<slug>[\w-]+)$', 'pages.views.view_page', name='pages_view_page'),
     url(r'^page/(?P<slug>[\w-]+)/revision/(?P<revision_id>\d+)$', 'pages.views.view_page', name='pages_view_page'),
-    url(r'^page/(?P<category_id>\d+)/new$', 'pages.views.edit_page', name='pages_new_page'),
-    url(r'^page/(?P<page_id>\d+)/edit$', 'pages.views.edit_page', name='pages_edit_page'),
-    url(r'^page/(?P<page_id>\d+)/delete', 'pages.views.delete_page', name='pages_delete_page')
+    url(r'^new_page/(?P<category_id>\d+)$', 'pages.views.edit_page', name='pages_new_page'),
+    url(r'^new_page$', 'pages.views.edit_page', name='pages_new_page'),
+    url(r'^edit_page/(?P<page_id>\d+)$', 'pages.views.edit_page', name='pages_edit_page'),
+    url(r'^delete_page/(?P<page_id>\d+)', 'pages.views.delete_page', name='pages_delete_page')
 )
