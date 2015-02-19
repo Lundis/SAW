f9d72b4e5f2fb9116329660b23b9e158885d47dd
Lundis <lundis_lundis@hotmail.com>
2015-02-19 13:51:09 +0200

    Documented the ajax view for frontpage. Also added a permission check(!)

--- studassweb/frontpage/ajax.py
+++ studassweb/frontpage/ajax.py
@@ -1,10 +1,20 @@
 from django.http import HttpResponseNotAllowed
 from django_ajax.decorators import ajax
 from .forms import PlacementForm
+from users.decorators import has_permission
+from .register import CAN_EDIT_FRONTPAGE
 
 
 @ajax
+@has_permission(CAN_EDIT_FRONTPAGE)
 def move_item(request):
+    """
+    POST variables: item_id, ordering_index, location
+    ordering_index > 0
+    location in (FrontPageItem.MAINBAR, FrontPageItem.SIDEBAR, FrontPageItem.HIDDEN)
+    :param request:
+    :return:
+    """
     if not request.method == 'POST':
         return HttpResponseNotAllowed(['POST'])
 
