a2d2b99a53dd1295b84bc2b2e3a9686304329e23
Lundis <lundis.bah@gmail.com>
2015-02-22 23:50:52 +0200

    Rewrote most of the menu editor form to work with any kind of data that needs to be sorted between different categories.

--- studassweb/base/fields.py
+++ studassweb/base/fields.py
@@ -1,4 +1,5 @@
 from ckeditor.fields import RichTextField, RichTextFormField
+from django import forms
 from .html_tag_closer import complete_html
 
 
@@ -36,3 +37,35 @@ class ValidatedRichTextFormField(RichTextFormField):
         cleaned = super(ValidatedRichTextFormField, self).clean(value)
         html, closing_tags = complete_html(cleaned)
         return html + closing_tags
+
+
+class HiddenModelField(forms.IntegerField):
+    """
+    A menu field for interpreting the order of a model
+    POST.field.name must be in the form *-<id>, where id is a valid id
+    POST.field.value must be an integer >= 0
+    """
+    def __init__(self, *args, **kwargs):
+        name = kwargs.pop('name')
+        model = kwargs.pop('model')
+        super(HiddenModelField, self).__init__(*args, **kwargs)
+        self.name = name
+        self.model = model
+
+    def clean(self, value):
+        """
+
+        :param value: The value attribute of the field
+        :return: clean value
+        """
+        cleaned_num = super(HiddenModelField, self).clean(value)
+        # Check that the id is positive and that the menu item actually exists
+        if cleaned_num < 0:
+            raise forms.ValidationError("Menu item index below 0")
+        # assume that the form has only added proper fields,
+        # so we only check that the last part of the name is a valid menu item id
+        item_id = int(self.name.split('-')[-1])
+        if self.model.objects.filter(id=item_id).count() != 1:
+            raise forms.ValidationError("%s with id %d does not exist!", self.model, item_id)
+        # else return the cleaned menu index
+        return cleaned_num
