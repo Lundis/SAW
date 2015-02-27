b4c1543658fcfef3946b8bf171b7e5961352c7f4
Oscar Hellsten <oshellst@abo.fi>
2015-02-27 17:07:05 +0200

    commiting so magnus can take a look at gallery!

--- studassweb/gallery/models.py
+++ studassweb/gallery/models.py
@@ -10,9 +10,6 @@ class Album(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     modified = models.DateTimeField(auto_now=True)
 
-    def save(self, *args, **kwargs): ##could remove this
-        super(Album, self).save(*args, **kwargs)
-
     def get_absolute_url(self):
         return reverse("gallery.views.view_album", kwargs={'album_id': self.id})
 
@@ -26,19 +23,11 @@ class Photo(models.Model):
     album_id = models.ForeignKey(Album)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     description = models.TextField(max_length=300)
-    uploaded = models.DateTimeField()
+    uploaded = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='gallery_files')
 
     def get_absolute_url(self):
         return reverse("gallery.views.view_picture", kwargs={'photo_id': self.id})
 
     def __str__(self): ##this is not a good way to do it
         return self.uploaded.strftime("%Y-%m-%d") + " : " + str(self.album_id)
-
-
-class PhotoFile(models.Model):
-    image = models.ImageField(upload_to='gallery_files')
-    photo_id = models.ForeignKey(Photo)
-
-    def __str__(self):
-        return self.image.name
-
