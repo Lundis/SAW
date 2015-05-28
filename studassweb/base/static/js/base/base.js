496ba2246546731de8ea3468fbd477c5508f8321
Lundis <lundis.bah@gmail.com>
2015-05-28 20:08:29 +0300

    new: nested menu items.

--- studassweb/base/static/js/base/base.js
+++ studassweb/base/static/js/base/base.js
@@ -4,3 +4,22 @@ $("div[data-toggle=hide]").click(function(event) {
     $(button).toggleClass("active");
     $("." + target_class).toggleClass("hidden");
 });
+
+// This supposedly makes submenus clickable on touch devices
+$(function(){
+	$(".dropdown-menu > li > a.trigger").on("click",function(e){
+		var current=$(this).next();
+		var grandparent=$(this).parent().parent();
+		if($(this).hasClass('left-caret')||$(this).hasClass('right-caret'))
+			$(this).toggleClass('right-caret left-caret');
+		grandparent.find('.left-caret').not(this).toggleClass('right-caret left-caret');
+		grandparent.find(".sub-menu:visible").not(current).hide();
+		current.toggle();
+		e.stopPropagation();
+	});
+	$(".dropdown-menu > li > a:not(.trigger)").on("click",function(){
+		var root=$(this).closest('.dropdown');
+		root.find('.left-caret').toggleClass('right-caret left-caret');
+		root.find('.sub-menu:visible').hide();
+	});
+});
